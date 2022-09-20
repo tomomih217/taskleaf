248502b542a076c8d6d533dbaa932b8acb1ac4ec
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-20 11:09:43 +0900

    add user edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,6 +22,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update!(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
