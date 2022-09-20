19a8190ee2dd9c1ab6fc254ac9083d9d3fcee7ca
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-20 10:53:34 +0900

    add user index and show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,12 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -14,12 +22,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-  end
-
-  def show
-  end
-
-  def index
   end
 
   private
