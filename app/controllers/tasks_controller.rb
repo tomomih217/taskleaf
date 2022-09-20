8ce620be465eca81dae1fc42434e4cd134a9e2ea
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-20 12:45:49 +0900

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,17 +22,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find_by(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find_by(params[:id])
+    task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find_by(params[:id])
+    task = Task.find(params[:id])
     task.destroy
     redirect_to task_url, notice: "タスク「#{task.name}」を削除しました。"
   end
