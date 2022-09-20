d64bd1dab1538780a5ef6a669428d65ae764f3a2
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-20 12:00:43 +0900

    add user update and destroy

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,7 +15,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を登録しました。"
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
@@ -27,11 +27,17 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update!(user_params)
+    if @user.update(user_params)
       redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :edit
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
