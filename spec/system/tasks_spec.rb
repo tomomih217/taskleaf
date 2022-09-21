7635f434908cc2688d5abbc4e84ff773419c25f7
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-21 14:23:47 +0900

    success to check task index and show

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-    describe '一覧表示機能' do
-        before do
-            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') 
-            #create:データベースに保存　build:保存しない
-            FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-        end
+    let(:user_a){ FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b){ FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+    #create:データベースに保存　build:保存しない
+    let!(:task_a){ FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
+    before do
+        visit login_path
+        fill_in 'メールアドレス', with: login_user.email
+        fill_in 'パスワード', with: login_user.password
+        click_button 'ログインする'
+    end
+
+    describe '一覧表示機能' do 
         context 'ユーザーAがログインしている時' do
-            before do
-                visit login_path
-                fill_in 'メールアドレス', with: 'a@example.com'
-                fill_in 'パスワード', with: 'password'
-                click_button 'ログインする'
-            end
+            let(:login_user){ user_a }
 
             it 'ユーザーAが作成したタスク一覧が表示される' do
                 expect(page).to have_content '最初のタスク'
             end
         end
-    end 
+
+        context 'ユーザーBがログインしている時' do
+            let(:login_user){ user_b }
+
+            it 'ユーザーAが作ったタスクが表示されない' do
+                #画面上にユーザーAが作ったタスクが表示されない
+                expect(page).to have_no_content '最初のタスク'
+            end
+        end
+    end
+    
+    describe '詳細表示機能' do
+        context 'ユーザーAがログインしている時' do
+            let(:login_user){ user_a }
+            before do
+                #詳細画面に移動
+                visit task_path(task_a)
+            end
+
+            it 'ユーザーAが作成したタスクが表示される' do
+                #画面上にユーザーAのタスクが表示されているか確認
+                expect(page).to have_content '最初のタスク'
+            end
+        end
+
+    end
 end
